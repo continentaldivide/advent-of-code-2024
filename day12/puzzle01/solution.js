75591f757a92fbca8f52c78932f1fa9cf2f2f25b
Andrew Smith <andrew@andrewsmith.org>
2025-03-03 13:32:19 -0500

    Continued work on day 12 puzzle 1

--- day12/puzzle01/solution.js
+++ day12/puzzle01/solution.js
@@ -18,16 +18,57 @@ const solution = async () => {
   const data = await fs.readFile("./input2.txt", "utf8");
   let dataArray = data.split(/\r\n|\r|\n/);
   let visitedTiles = {};
-  console.log(dataArray);
+  let totalCost = 0;
+
+  const recurse = (currentPlant, i, j, memo = { area: 0, perimeter: 0 }) => {
+    if (currentPlant === "B") {
+      console.log(memo.area, i, j);
+    }
+    visitedTiles[`${i}.${j}`] = true;
+    memo.area += 1;
+    let left = [i, j - 1];
+    let right = [i, j + 1];
+    let up = [i - 1, j];
+    let down = [i + 1, j];
+    let adjacentCoords = [left, right, up, down];
+    let validAdjacents = [];
+    adjacentCoords.forEach((coords) => {
+      if (visitedTiles[`${coords[0]}.${coords[1]}`]) {
+        return;
+      }
+      const inBounds = coords.every(
+        (coord) => coord >= 0 && coord < dataArray.length
+      );
+      if (!inBounds) {
+        memo.perimeter += 1;
+        return;
+      }
+      if (dataArray[coords[0]][coords[1]] !== currentPlant) {
+        memo.perimeter += 1;
+        return;
+      }
+      validAdjacents.push(coords);
+    });
+    currentPlant === "B" ? console.log(memo.area, validAdjacents) : null;
+    if (validAdjacents.length === 0) {
+      return memo;
+    }
+    validAdjacents.forEach((coords) => {
+      recurse(currentPlant, coords[0], coords[1], memo);
+    });
+    return memo;
+  };
 
   dataArray.forEach((line, i) => {
     for (let j = 0; j < line.length; j++) {
       if (visitedTiles[`${i}.${j}`]) {
         continue;
       }
-      // do something
+      let { area, perimeter } = recurse(line[j], i, j);
+      totalCost += area * perimeter;
     }
   });
+  console.log(totalCost);
 };
 
 solution();
